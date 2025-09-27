fa7e2d5a410bb375290caee5e8801dc785ddd367
Mayank Shukla <shukla.mayank247@gmail.com>
2025-09-27 19:40:25 +0530

    added auth development for authorisation for signIn

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -88,6 +88,7 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/auth/signin",
   },
+  debug: process.env.NODE_ENV === 'development',
 };
 
 const handler = NextAuth(authOptions);
